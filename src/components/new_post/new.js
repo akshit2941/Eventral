940522530281aa98a26f2a3b19866259cd8eb677
akshit2941 <sainiakshit2941@gmail.com>
2024-04-07 19:05:34 +0530

    event upload

--- src/components/new_post/new.js
+++ src/components/new_post/new.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types'; // Import PropTypes
 import './new.css';
 import 'firebase/storage';
 import { getFirestore, addDoc, collection } from "firebase/firestore";
+import { storage } from '../firebase/firebase';
+import { v4 } from "uuid";
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL,
+    listAll,
+} from 'firebase/storage';
 
 function NewPost({ setOpenModal }) {
-    // State to manage form input values
     const [eventTitle, setEventTitle] = useState('');
     const [eventDescription, setEventDescription] = useState('');
     const [eventCategory, setEventCategory] = useState('');
     const [eventTags, setEventTags] = useState('');
-    const [mediaFile, setMediaFile] = useState('');
+    const [mediaFile, setMediaFile] = useState(null);
+    const [imageUrls, setImageUrls] = useState([]);
 
     const db = getFirestore();
-
-
-
+    const imagesListRef = ref(storage, "images/");
 
     const saveDataToFirestore = async () => {
         await addDoc(collection(db, "newEvent"), {
@@ -23,16 +29,37 @@ function NewPost({ setOpenModal }) {
             eventDescription,
             eventCategory,
             eventTags,
+            imageUrl: imageUrls
         });
 
         console.log("Document written to Database");
     };
 
-    // Function to handle file input change
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        setMediaFile(file);
+    const handlePublish = async () => {
+        saveDataToFirestore();
+        uploadFile();
+        setOpenModal(false);
     };
+
+    const uploadFile = async () => {
+        if (mediaFile == null) return;
+        const imageRef = ref(storage, `images/${mediaFile.name + v4()}`);
+        uploadBytes(imageRef, mediaFile).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+                setImageUrls((prev) => [...prev, url]);
+            });
+        });
+    };
+
+    useEffect(() => {
+        listAll(imagesListRef).then((response) => {
+            response.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageUrls((prev) => [...prev, url]);
+                });
+            });
+        });
+    }, []);
 
 
     return (
@@ -44,66 +71,66 @@ function NewPost({ setOpenModal }) {
                 <div className="main-container">
                     <h1 className="head">Event Details</h1>
                     <div className="container-details">
-                        <form>
-                            <h1 className="head-title">Event Title</h1>
-                            <input
-                                type="text"
-                                className="event-tile-input-area"
-                                placeholder="Event name"
-                                value={eventTitle}
-                                onChange={(e) => setEventTitle(e.target.value)}
-                            />
+                        <h1 className="head-title">Event Title</h1>
+                        <input
+                            type="text"
+                            className="event-tile-input-area"
+                            placeholder="Event name"
+                            value={eventTitle}
+                            onChange={(e) => setEventTitle(e.target.value)}
+                        />
 
-                            <h1 className="head-title head-margin">Event Description</h1>
-                            <input
-                                type="text"
-                                className="event-tile-input-area"
-                                placeholder="Add a brief desciption"
-                                value={eventDescription}
-                                onChange={(e) => setEventDescription(e.target.value)}
-                            />
+                        <h1 className="head-title head-margin">Event Description</h1>
+                        <input
+                            type="text"
+                            className="event-tile-input-area"
+                            placeholder="Add a brief desciption"
+                            value={eventDescription}
+                            onChange={(e) => setEventDescription(e.target.value)}
+                        />
 
-                            <h1 className="head-title head-margin">Event Category</h1>
-                            <input
-                                type="text"
-                                className="event-tile-input-area"
-                                placeholder="Add Event Category"
-                                value={eventCategory}
-                                onChange={(e) => setEventCategory(e.target.value)}
-                            />
+                        <h1 className="head-title head-margin">Event Category</h1>
+                        <input
+                            type="text"
+                            className="event-tile-input-area"
+                            placeholder="Add Event Category"
+                            value={eventCategory}
+                            onChange={(e) => setEventCategory(e.target.value)}
+                        />
 
-                            <h1 className="head-title head-margin">Event tags</h1>
-                            <input
-                                type="text"
-                                className="event-tile-input-area"
-                                placeholder="Enter Tags...."
-                                value={eventTags}
-                                onChange={(e) => setEventTags(e.target.value)}
-                            />
+                        <h1 className="head-title head-margin">Event tags</h1>
+                        <input
+                            type="text"
+                            className="event-tile-input-area"
+                            placeholder="Enter Tags...."
+                            value={eventTags}
+                            onChange={(e) => setEventTags(e.target.value)}
+                        />
 
-                            <h1 className="head-title head-margin">Upload Media</h1>
-                            <input
-                                type="file"
-                                className="media-select"
-                                onChange={handleFileChange}
-                            />
+                        <h1 className="head-title head-margin">Upload Media</h1>
+                        <input
+                            type="file"
+                            className="media-select"
+                            onChange={(event) => {
+                                setMediaFile(event.target.files[0]);
+                            }}
+                        />
 
-                            <div className="preview-container">
-                                <div className="media-preview">
-                                    <h1 className="preview-head-main">Event Preview</h1>
-                                    <h1 className="preview-head">{eventTitle}</h1>
-                                    {mediaFile && <img src={URL.createObjectURL(mediaFile)} alt="" className="preview-img" />}
-                                    <p className="media-des event-desciption">{eventDescription}</p>
-                                </div>
+                        <div className="preview-container">
+                            <div className="media-preview">
+                                <h1 className="preview-head-main">Event Preview</h1>
+                                <h1 className="preview-head">{eventTitle}</h1>
+                                {mediaFile && <img src={URL.createObjectURL(mediaFile)} alt="" className="preview-img" />}
+                                <p className="media-des event-desciption">{eventDescription}</p>
                             </div>
+                        </div>
 
-                            <div className="submit-container">
-                                <div className="button-container">
-                                    <button className="submit-btn publish" type="submit" onClick={saveDataToFirestore}>Publish Event</button>
-                                    <button className="submit-btn draft">Save Draft</button>
-                                </div>
+                        <div className="submit-container">
+                            <div className="button-container">
+                                <button className="submit-btn publish" type="submit" onClick={handlePublish}>Publish Event</button>
+                                <button className="submit-btn draft">Save Draft</button>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
